647184d38dc8addef314b4b5eceb1f368afcf41f
DESKTOP-HACPIGP\maoudeh <moayadzangane@gmail.com>
2022-08-31 15:02:58 +0430

    This ended up not making the loop

---
+++ css/index.js
@@ -0,0 +1,22 @@
+let Card='<div class="container"><div class="header"><span class="name">نام: موید</span></div><div class="body"><span class="last-name">نام خانوادگی: زنگنه</span><span class="history"> تاریخ تولد: 1380/07/22</span></div><div class="footer"><span class="addres">آدرس: تهران - شهرری - میدان نارنج - کوچه محسن خداوردی - بن بست صدری - پلاک 13</span><span class="about"> کاربران سایت</span></div></div>'
+// let Name=["محمد","محسن","موید","نویان","حمید"]
+var Persons = [
+    {name: 'محمد' , family: 'یار احمدی' , history: '1380/07/22'},
+    {name: 'نویان' , family: 'یار احمدی' , history: '1380/07/22'},
+    {name: 'موید' , family: 'یار احمدی' , history: '1380/07/22'},
+    {name: 'محسن' , family: 'یار احمدی' , history: '1380/07/22'}
+]
+var personElement=document.getElementById("personsCard");
+Persons.forEach(function(element ,index ){
+    personElement.innerHTML+=Card//element.name;
+    var name2 = function(){
+        $(".name")
+        for(let x = 0; x<=Persons; x++){
+            
+            Persons.innerHTML
+        } 
+    }
+}
+)
+
+
